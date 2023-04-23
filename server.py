3e46d84b15a3ba4821b31cedb90d3d21227fe2f5
Souheil Khalfallh <khalfallah.souheil@planet.tn>
2023-04-23 07:59:50 +0100

    23/04/2023 at 7h59 am

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
-app = Flask(__name__)  
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__) 
+app.secret_key = 'keep it secret, keep it safe' 
 
 @app.route('/')         
 def index():
@@ -8,15 +9,30 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form['apple'])
-    return render_template("checkout.html",
-                           firstname=request.form['first_name'],
-                           lastname=request.form['last_name'],
-                           studentid=request.form['student_id'],
-                           strawberry=request.form['strawberry'],
-                           raspberry=request.form['raspberry'],
-                           apple=request.form['apple'],
-                           count=str(int(request.form['strawberry'])+int(request.form['raspberry'])+int(request.form['apple'])))
+    session['firstname']=request.form['first_name']
+    session['lastname']=request.form['last_name']
+    session['studentid']=request.form['student_id']
+    session['strawberry']=request.form['strawberry']
+    session['raspberry']=request.form['raspberry']
+    session['apple']=request.form['apple']
+    return redirect("/show")
+    
 
+@app.route("/show")
+def show_user():
+    print("Showing the User Info From the Form")
+    print(request.form)
+    # return render_template("show.html")
+    return render_template("checkout.html",
+                           firstname=session['firstname'],
+                           lastname=session['lastname'],
+                           studentid=session['studentid'],
+                           strawberry=session['strawberry'],
+                           raspberry=session['raspberry'],
+                           apple=session['apple'], 
+                           count=str(int(session['strawberry'])+int(session['raspberry'])+int(session['apple'])))
+
+    
 @app.route('/fruits')         
 def fruits():
     return render_template("fruits.html")
